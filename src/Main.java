f379dc04d1102ca78814a0694d70f01f32b971b4
Paweł Korniak <pawello1020@gmail.com>
2020-11-05 19:24:04 +0100

    classes bodies added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("hallo world");
-        Game tetris = new Game();
-        System.out.println(tetris.score);
+        ContactManager myContactManager = new ContactManager();
     }
 }

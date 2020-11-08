51544442c9827885b1e3c4f09cd24fe2db5df50a
Paweł Korniak <pawello1020@gmail.com>
2020-11-08 09:25:55 +0100

    searching is working correctly

--- src/Contact.java
+++ src/Contact.java
@@ -2,4 +2,8 @@ public class Contact {
     String name;
     String phoneNumber;
     String email;
+    Contact(String name,String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String [] args){
         ContactManager myContactManager = new ContactManager();
+        Contact tab[] = new Contact[10];
+        tab[0] = new Contact("pawel","111111111111");
+        tab[1] = new Contact("tomek","222222222222");
+        tab[2] = new Contact("maciek","333333333333");
+        tab[3] = new Contact("ola","4444444444444");
+
+        for(int i=0;i<4;i++){
+            myContactManager.addContact(tab[i]);
+        }
+        String searchName ="tomek";
+        System.out.println(searchName+" "+myContactManager.searchContact(searchName).phoneNumber);
+
+
     }
 }
